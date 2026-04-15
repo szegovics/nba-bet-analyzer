1cd600f473742717b9a8599f7bcefee909bf5ba5
szegovics <szgvrbnc@gmail.com>
2026-04-15 14:31:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -259,11 +259,10 @@ if analysis_mode == "🔥 Élő Prop Elemző":
 
     
 else:
-
     
-   matches = get_next_matchday_from_odds()
+    matches = get_next_matchday_from_odds()
     
-    if matches == []:
+    if matches = []:
         st.warning("Nincs elérhető meccs az Odds API-ban.")
     else:
         st.info(f"Dátum: következő játéknap| Talált meccsek száma: **{len(matches)}**")
